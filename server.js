0fbb678537bd885ed634ab7b6455abc575bfcdc0
Anusha Gali <anusha.gali03@gmail.com>
2024-12-05 11:39:38 -0500

    Update server.js

--- server.js
+++ server.js
@@ -11,16 +11,22 @@ const adminUserRoutes = require('./routes/users');
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5001;
 
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.error('MongoDB connection error:', err));
+const connectDB = async () => {
+    if (mongoose.connections[0].readyState) return;
+    await mongoose.connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+    console.log('MongoDB connected');
+};
+
+app.use(async (req, res, next) => {
+    await connectDB();
+    next();
+});
 
 app.use('/api/auth', authRoutes);
 app.use('/api/petlisting', petListingRoutes);
@@ -28,8 +34,6 @@ app.use('/api/users', userRoutes);
 app.use('/api/stats', statsRoutes);
 app.use('/api/admin/users', adminUserRoutes);
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
-
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+module.exports = app;
